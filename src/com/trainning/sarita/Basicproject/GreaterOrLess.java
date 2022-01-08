b5b7261c6fdcaeed7bd41cdfad6aa01b60514b89
sarita212 <96809104+sarita212@users.noreply.github.com>
2022-01-08 16:00:13 -0600

    "First Commit"

---
+++ src/com/trainning/sarita/Basicproject/GreaterOrLess.java
@@ -0,0 +1,19 @@
+package com.trainning.sarita.Basicproject;
+
+public class GreaterOrLess {
+    /**
+     *
+     * there will the four number like a,b,c,d and we add a and b to get e
+     * and add c and d to get f. finally we will compare e and f..
+     *
+     */
+
+    public void greaterThan(int e, int f) {
+        if (e>f) {
+            System.out.println("e is greater than f");
+        }
+        else {
+            System.out.println("a+b is lesser than c+d");
+        }
+    }
+}
---
+++ src/com/trainning/sarita/Basicproject/GreaterOrLessTest.java
@@ -0,0 +1,36 @@
+package com.trainning.sarita.Basicproject;
+import java.util.Scanner;
+
+public class GreaterOrLessTest {
+
+    public static void main(String[] args) {
+        GreaterOrLess eg = new GreaterOrLess();
+        Scanner sc= new Scanner(System.in);
+        System.out.println("enter your a number: ");
+        int a = sc.nextInt();
+        System.out.println("enter your b number: ");
+        int b = sc.nextInt();
+        System.out.println("enter your c number: ");
+        int c = sc.nextInt();
+        System.out.println("enter your d number: ");
+        int d = sc.nextInt();
+        System.out.println("The a number is " + a);
+        System.out.println("The b number is " + b);
+        System.out.println("The c number is " + c);
+        System .out.println("The d number is " + d);
+        int e = a + b; //initialization
+        int f = c + d;
+        System.out.println("The sum of a + b is: " + e);
+        System.out.println("The sum of c + d is: " + f);
+        eg.greaterThan(e,f);
+
+        System.out.println("-----------------------------------------------------");
+        System.out.println("enter your table: ");
+        int table = sc.nextInt();;
+        int i =1;
+        for( i =1; i<=10; i++) {
+            System.out.printf( "%d * %d= %d", table, i, table*i).println();
+
+        }
+    }
+}
