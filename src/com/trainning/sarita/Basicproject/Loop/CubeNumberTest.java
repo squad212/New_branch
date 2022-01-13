d38f8c32b1041294baa8193a00c29ce0191a99c5
sarita212 <96809104+sarita212@users.noreply.github.com>
2022-01-13 17:24:06 -0600

    sixth commit

---
+++ src/com/trainning/sarita/Basicproject/Loop/CubeNumberTest.java
@@ -0,0 +1,22 @@
+package com.trainning.sarita.Basicproject.Loop;
+
+import java.util.Scanner;
+
+public class CubeNumberTest {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        /// making a loop while will occur until user give the negative number
+
+        int number = -1;
+        do {
+            if (number != -1){
+                System.out.println("Cube is : " + (number * number * number));
+            }
+            System.out.println("Enter a number ");
+            number = sc.nextInt();
+        } while (number >= 0);
+        System.out.println("Thank You");
+    }
+}
---
+++ src/com/trainning/sarita/Basicproject/WhileLoop/WhileNumberPlayerTest.java
@@ -0,0 +1,12 @@
+package com.trainning.sarita.Basicproject.WhileLoop;
+
+public class WhileNumberPlayerTest {
+
+    public static void main(String[] args) {
+        WhileNumberPlayer player = new WhileNumberPlayer(30);// limit is 30
+        player.printSquaresUptoLimit();
+        player.printCubesUptoLimit();
+
+
+    }
+}
