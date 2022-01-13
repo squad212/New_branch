91c75e78e2d8a7d8c558fda44e3967df152a4270
sarita212 <96809104+sarita212@users.noreply.github.com>
2022-01-13 15:40:40 -0600

    fifth commit

---
+++ src/com/trainning/sarita/Basicproject/LoopExample/MyNumber.java
@@ -0,0 +1,52 @@
+package com.trainning.sarita.Basicproject.LoopExample;
+
+public class MyNumber {
+
+    private int number;
+
+    public MyNumber(int number) {
+        this.number = number;
+    }
+
+    public boolean isPrime() {
+        // Because negative and 1 cant be prime number
+        if (number < 2) {
+            return false;
+        }
+        // use the % modulos which is reminder
+        for (int i = 2; i <= number - 1; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public int sumUptoN() {
+        int sum = 0;
+        for (int i = 1; i <= number; i++) {
+            sum = sum + i;
+        }
+        return sum;
+    }
+
+    public int sumOfDivisors() {
+        int sum = 0;
+        for (int i = 2; i < number; i++)
+            if (number % i == 0) {
+                sum = sum + i;
+            }
+        return sum;
+    }
+
+
+    public void numberTriangle() {
+
+        for (int i = 1; i <= number; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();// it helps to print line in different line.
+        }
+    }
+}
---
+++ src/com/trainning/sarita/Basicproject/LoopExample/MyNumberTest.java
@@ -0,0 +1,30 @@
+package com.trainning.sarita.Basicproject.LoopExample;
+
+public class MyNumberTest {
+    public MyNumberTest() {
+        super();
+    }
+
+    public static void main(String[] args) {
+        MyNumber number = new MyNumber(5);
+
+        // for prime number
+        boolean isPrime = number.isPrime();
+        System.out.println("The Number is Prime : " + isPrime);
+
+        // for sum of the number
+        int sum = number.sumUptoN();
+        System.out.println("sum of the numbers : " + sum);
+
+        // sum of all the divisor except 1 and itself
+        int sumOfDivisors = number.sumOfDivisors();
+        System.out.println("The sum of Divisors : " + sumOfDivisors);
+
+        // Making tree of the number
+        number.numberTriangle();
+
+
+
+
+    }
+}
